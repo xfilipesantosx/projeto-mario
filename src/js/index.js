e4f300befb630d1c04afac02969c3946b9f4ad67
Filipe Santos Araujo <filipe.santos.araujo@gmail.com>
2023-01-21 22:21:02 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const botaoTrailer = document.querySelector(".botao-trailer")
+const botaoFecharModal = document.querySelector(".fechar-modal")
+const modal = document.querySelector(".modal")
+const video = document.getElementById("video")
+const linkDoVideo = video.src
+
+function alternarModal() {
+    modal.classList.toggle("aberto")
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal()
+    video.setAttribute("src", linkDoVideo)
+    
+})
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal()
+    video.setAttribute("src", "")
+    
+})
+
+
